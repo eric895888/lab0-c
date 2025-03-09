6866dd3a05101744367dedeb65bb87eb28e58933
shan <eric895888@gmail.com>
2025-03-10 00:01:07 +0800

    Implement a doubly linked list
    Implement the functionality for adding and deleting elements in a
    doubly linked list, as well as calculating the number of elements.
    
    Change-Id: Iec2334bed3f60cd0cc516ed145a3b03dad6b840f

--- queue.c
+++ queue.c
@@ -4,43 +4,138 @@
 
 #include "queue.h"
 
+
+
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *new_node = malloc(sizeof(struct list_head));
+
+    if (!new_node) {
+        return NULL;  // If memory allocation fails, returns NULL.
+    }
+
+    new_node->prev = new_node;
+    new_node->next = new_node;
+
+    return new_node;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head || head->next == head) {
+        return;
+    }
+
+    struct list_head *curr = head->next;
+    struct list_head *tmp = NULL;
+
+    while (curr != head) {
+        tmp = curr->next;
+        free(curr);
+        curr = tmp;
+    }
+
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head || !s)
+        return false;
+
+    element_t *new_node = (element_t *) malloc(sizeof(element_t));
+    if (!new_node)
+        return false;
+
+    new_node->value = strdup(s);
+    if (!new_node->value) {
+        free(new_node);
+        return false;
+    }
+
+    new_node->list.next = head->next;
+    new_node->list.prev = head;
+    head->next->prev = &new_node->list;
+    head->next = &new_node->list;
+
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head || !s)
+        return false;
+
+    element_t *new_node = (element_t *) malloc(sizeof(element_t));
+    if (!new_node)
+        return false;
+
+    new_node->value = strdup(s);
+    if (!new_node->value) {
+        free(new_node);
+        return false;
+    }
+    new_node->list.next = head;
+    new_node->list.prev = head->prev;
+    head->next->prev = &new_node->list;
+    head->next = &new_node->list;
+
     return true;
 }
 
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || head->next == head)
+        return NULL;
+
+    element_t *node = (element_t *) head->next;
+    head->next = node->list.next;
+    node->list.next->prev = head;
+
+    if (sp && node->value) {
+        strncpy(sp, node->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    return node;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || head->prev == head)
+        return NULL;
+
+    element_t *node = (element_t *) head->prev;
+    head->prev = node->list.prev;
+    node->list.prev->next = head;
+
+    if (sp && node->value) {
+        strncpy(sp, node->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    return node;
 }
 
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head)
+        return 0;
+
+    int count = 0;
+    struct list_head *curr = head->next;
+    while (curr != head) {
+        count++;
+        curr = curr->next;
+    }
+    return count;
 }
 
 /* Delete the middle node in queue */
